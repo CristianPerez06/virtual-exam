045db46c0dcd7b81ad56caed0732eeb623c64431
Cristian Perez <cristian.ap84@gmail.com>
2020-12-25 19:46:59 -0300

    Added momentjs. Fixed initial state.

--- admin-app/src/common/constants.js
+++ admin-app/src/common/constants.js
@@ -1,15 +1,16 @@
+export const TIME = {
+  A_MINUTE: 60,
+  HALF_HOUR: 1800,
+  AN_HOUR: 3600
+}
+
 export const ACCOUNT_ACTION_TYPES = {
   LOGIN: 'LOGIN',
-  LOGOUT: 'LOGOUT'
+  LOGOUT: 'LOGOUT',
+  REFRESH: 'REFRESH'
 }
 
 export const COGNITO_CODES = {
   NOT_AUTHORIZED: 'NotAuthorizedException',
   NEW_PASSWORD_REQUIRED: 'newPasswordRequired'
-}
-
-
-export const COOKIE_EXPIRATION = {
-  HALF_HOUR: 1800,
-  AN_HOUR: 3600
 }
